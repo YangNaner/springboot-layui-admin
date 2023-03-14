f7586ea6babf566b51e363ace057934a4183232a
YangNaner <312913666@qq.com>
2023-03-15 02:50:18 +0800

    首次提交

---
+++ README.md
@@ -0,0 +1,49 @@
+# 基于SpringBoot与LayUI的后台管理系统
+一套SpringBoot+MyBatis+FreeMarker+LayUI的后台管理系统.
+适用于**小体量,CRUD业务为主的后台系统快速搭建和学习**. 
+在学习过程中发现很多地方网上没有给出一个整套的案例, 因此进行开源.
+
+由于时间仓促, 没有加入权限验证和Redis等功能.但对于SpringBoot+LayUI开发来说是一个很好的例子.
+
+![](https://note.youdao.com/yws/public/resource/58918c59a59ab57824426ac85e456ea7/xmlnote/35F174830F724A89AAC2B1E3BB20B455/4953)
+![](https://note.youdao.com/yws/public/resource/58918c59a59ab57824426ac85e456ea7/xmlnote/522EA345E7774CEAA9FC6242FD6ABD7B/4957)
+![](https://note.youdao.com/yws/public/resource/58918c59a59ab57824426ac85e456ea7/xmlnote/F8C9999E4F2B4CF58D66E039DB001899/4959)
+## 使用方法 How To Use
+1. 在MySQL(作者5.7)中创建数据库, 导入admin.sql文件至数据库.
+2. 导入Maven工程
+3. 修改相关配置application.properties
+4. 在userauth表中添加测试帐号
+5. 在D盘根目录下创建hrm-upload文件夹,用于文件上传 (可通过配置文件更改)
+6. 运行Application.java
+7. 访问http://localhost/login 登录即可使用
+
+## 特色/解决的一些问题:
+### 后端:
+- 抽取CRUD公共业务层和DAO, 结合反射, 实现了公共CRUD抽取(继承+Mapper配置即可使用)
+- 使用SqlSessionTemplate进行Mapper查找和执行
+- 遵循RestFul API设计规范, 结合FreeMarker进行前后端分离
+- Interceptor+Session检查登录状态
+
+### 前端:
+- iframe实现页面局部刷新
+- Layui数据表格内嵌下拉框问题
+- Layer弹窗Form组件渲染不出来的问题(使用回调函数)
+- Layui数据表格分页问题
+- 实时模糊匹配查询/数据表格重新渲染
+- Layui+SpringBoot文件下载
+
+## 包含模块:
+- 权限管理
+- 部门管理
+- 职位管理
+- 员工管理
+- 部门管理
+- 下载中心
+
+## 通用CRUD类图
+![](https://imgconvert.csdnimg.cn/aHR0cHM6Ly9ub3RlLnlvdWRhby5jb20veXdzL3B1YmxpYy9yZXNvdXJjZS81ODkxOGM1OWE1OWFiNTc4MjQ0MjZhYzg1ZTQ1NmVhNy94bWxub3RlL0JBRDk2MTY2QkRBRjRERUFBMzRERTkyM0Q0Q0RCQkNFLzQ5MzA?x-oss-process=image/format,png)
+
+注: 登录页面的背景图片引用自知乎首页, 请自行更换.
+
+有任何问题或者建议, 欢迎联系交流!
+
---
+++ hrm.sql
@@ -0,0 +1,128 @@
+/*
+SQLyog Ultimate v11.25 (64 bit)
+MySQL - 8.0.13 : Database - hrm
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`hrm` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+
+USE `hrm`;
+
+/*Table structure for table `department` */
+
+DROP TABLE IF EXISTS `department`;
+
+CREATE TABLE `department` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '部门名称',
+  `description` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '详细信息',
+  `created_time` datetime DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+/*Data for the table `department` */
+
+insert  into `department`(`id`,`name`,`description`,`created_time`) values (12,'人事部','testtesttesttesttest','2023-03-14 16:36:53');
+
+/*Table structure for table `employee` */
+
+DROP TABLE IF EXISTS `employee`;
+
+CREATE TABLE `employee` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '姓名',
+  `sex` tinyint(1) DEFAULT NULL COMMENT '性别',
+  `phone` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '手机号码',
+  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '邮箱',
+  `education` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '学历',
+  `idcard` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '身份证',
+  `address` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '联系地址',
+  `position_id` int(11) DEFAULT NULL COMMENT '职位ID',
+  `dept_id` int(11) DEFAULT NULL COMMENT '部门ID',
+  `created_time` datetime DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+/*Data for the table `employee` */
+
+/*Table structure for table `fileupload` */
+
+DROP TABLE IF EXISTS `fileupload`;
+
+CREATE TABLE `fileupload` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '标题',
+  `description` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '描述',
+  `filename` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `path` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '上传人',
+  `created_time` datetime DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+/*Data for the table `fileupload` */
+
+insert  into `fileupload`(`id`,`title`,`description`,`filename`,`path`,`username`,`created_time`) values (6,'xx','111','wwedu.top.png','D:/hrm-upload/7588bf25-a878-4241-bdf9-72ac5ff1f67c.png','ww','2023-03-14 16:39:34');
+
+/*Table structure for table `position` */
+
+DROP TABLE IF EXISTS `position`;
+
+CREATE TABLE `position` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '职位名称',
+  `description` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '详细信息',
+  `created_time` datetime DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+/*Data for the table `position` */
+
+insert  into `position`(`id`,`name`,`description`,`created_time`) values (5,'Java开发工程师','996','2023-03-14 16:37:16');
+
+/*Table structure for table `post` */
+
+DROP TABLE IF EXISTS `post`;
+
+CREATE TABLE `post` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+  `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '公告标题',
+  `content` text CHARACTER SET utf8 COLLATE utf8_general_ci COMMENT '内容',
+  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '发起人名称',
+  `created_time` datetime DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+/*Data for the table `post` */
+
+insert  into `post`(`id`,`title`,`content`,`username`,`created_time`) values (5,'test','123','ww','2023-03-14 16:36:06');
+
+/*Table structure for table `userauth` */
+
+DROP TABLE IF EXISTS `userauth`;
+
+CREATE TABLE `userauth` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '用户名',
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '密码',
+  `is_admin` tinyint(1) DEFAULT NULL COMMENT '是否为管理员',
+  `created_time` datetime DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+/*Data for the table `userauth` */
+
+insert  into `userauth`(`id`,`username`,`password`,`is_admin`,`created_time`) values (16,'ww','123456',1,NULL),(17,'张三','123',1,'2023-03-14 16:36:23');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
